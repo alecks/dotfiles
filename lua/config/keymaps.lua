d6cf18ffc5901cee70a0863c45f7e02e7a49c92e
Alex P <alexmpedley@gmail.com>
2024-05-03 16:20:10 +0100

    add half pgdn/up keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 local map = vim.keymap.set
 map("n", "<leader>\\", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
