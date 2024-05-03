4a1e62a0d6829da038799d3576d9672968b33b25
Alex P <alexmpedley@gmail.com>
2024-05-03 17:28:10 +0100

    add nerd fonts to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-brew install nvim tmux kitty ripgrep lazygit
+curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/NerdFontsSymbolsOnly/SymbolsNerdFont-Regular.ttf -o ~/Library/Fonts/SymbolsNerdFont-Regular.ttf
+brew tap homebrew/cask-fonts
+brew install nvim tmux kitty ripgrep lazygit font-iosevka
 ln -s ~/.config/tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "DONE"
