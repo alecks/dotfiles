8432de2ecdb300fa1bc8ea93423873b60a6c0543
Alex P <alexmpedley@gmail.com>
2024-05-15 20:51:26 +0100

    update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
 return {
-  "rebelot/kanagawa.nvim",
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = true,
+        styles = {
+          floats = "dark",
+          sidebars = "dark",
+        },
+      })
+    end,
+  },
   { "LazyVim/LazyVim", opts = {
-    colorscheme = "kanagawa-dragon",
+    colorscheme = "tokyonight",
   } },
 }
