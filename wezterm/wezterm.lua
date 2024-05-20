e56a2f9ab84b5e2ff581450234a6b4df840c7795
Alex P <alexmpedley@gmail.com>
2024-05-20 19:24:55 +0100

    replace kitty with wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+local config = {
+	font = wezterm.font("Iosevka Term", { weight = "Medium" }),
+	font_size = 15,
+
+	color_scheme = "rose-pine",
+}
+
+return config
