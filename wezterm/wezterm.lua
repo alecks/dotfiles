fba6d13db94aa4cafe1e62bb574f23c9c736bf40
Alex Pedley <alex@pedley.org>
2024-06-25 12:36:32 +0100

    add kitty back

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = {
-	font = wezterm.font("Iosevka Term", { weight = "Medium" }),
+	font = wezterm.font("Iosevka", { weight = "Medium" }),
 	font_size = 15,
 
 	color_scheme = "rose-pine",
