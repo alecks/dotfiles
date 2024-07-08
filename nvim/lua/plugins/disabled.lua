7b2cd70d33555dfbff440ac0b736e45eb9b0a547
Alex Pedley <alex@pedley.org>
2024-07-08 16:04:29 +0100

    disable inlay_hints, not lspconfig

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "neovim/nvim-lspconfig", opts = { enabled = false } },
+  { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
 }
