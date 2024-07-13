c0d99b112e860e9493a4f3e4e933cd662908770b
Alex Pedley <alex@pedley.org>
2024-07-13 07:58:48 +0100

    disable documentHighlightProvider

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,17 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+local disable_semantic_highlighting = {
+  zig = false, -- zigs is sometimes very slow, keeping enabled for now
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+    local filetype = vim.bo[bufnr].filetype
+    if disable_semantic_highlighting[filetype] then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+
+    client.server_capabilities.documentHighlightProvider = nil
+  end,
+})
