d8fc7fa784d7c4f3d0116b264b15c6cf2a3ca104
Alex Pedley <alex@pedley.org>
2024-07-21 13:12:24 +0100

    change presence to a fork

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,5 +1,5 @@
 return {
-  "andweeb/presence.nvim",
+  "jiriks74/presence.nvim",
   { "neovim/nvim-lspconfig", opts = { inlay_hints = { enabled = false } } },
   { "folke/flash.nvim", opts = { modes = { search = { enabled = true } } } },
 }
