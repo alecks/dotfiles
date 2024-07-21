3974d7acf73e2271dcc6294621efc4af4f8e9c01
Alex Pedley <alex@pedley.org>
2024-07-21 13:12:00 +0100

    enable trouble

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,6 +3,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
-  { "folke/trouble.nvim", enabled = false }, -- i just use <leader>sd to search diagnostics with telescope
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
