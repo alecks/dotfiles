b53709559ab482506738ff5d7315fa8e84d53ed9
Alex Pedley <alex@pedley.org>
2024-07-21 13:12:17 +0100

    add flow theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,16 @@
 return {
-  "cdmill/neomodern.nvim",
+  "tjdevries/colorbuddy.nvim",
+  {
+    "0xstepit/flow.nvim",
+    opts = function()
+      require("flow").setup({
+        transparent = false,
+        fluo_color = "pink",
+        mode = "normal",
+      })
+    end,
+  },
   { "LazyVim/LazyVim", opts = {
-    colorscheme = "roseprime",
+    colorscheme = "flow",
   } },
 }
