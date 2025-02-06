11f2a051548d58312afaa57ed2abc79c970cc0d1
Alex Pedley <alex@pedley.org>
2025-02-06 22:41:19 +0000

    format wezterm conf

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,8 +5,8 @@ local config = wezterm.config_builder()
 config.color_scheme = "Poimandres"
 
 config.font = wezterm.font_with_fallback({
-	"Berkeley Mono",
-	-- 	"Symbols Nerd Font Mono",
+  "Berkeley Mono",
+  -- 	"Symbols Nerd Font Mono",
 })
 config.font_size = 18
 
@@ -15,10 +15,10 @@ config.tab_bar_at_bottom = true
 config.enable_scroll_bar = false
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 return config
